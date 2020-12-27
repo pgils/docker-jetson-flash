6122e43871a8fb2cbe80b5c46b31d8cae76d7492
Pelle van Gils <pelle@vangils.xyz>
2020-12-27 20:37:56 +0100

    Initial version

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+[ "${DEBUG:=false}" = true ] && set -o xtrace
+
+# Update the 'size' property for the resin-root partitions
+if [ -n "${ROOTFS_SIZE:-}" ]; then
+    xmlstarlet ed --inplace -u \
+        "/partition_layout/device/partition[starts-with(@name, 'resin-root')]/size" \
+        -v "$ROOTFS_SIZE" \
+        jetson-flash/assets/"$MACHINE"-assets/resinOS-flash.xml
+fi
+
+/jetson-flash/bin/cmd.js "$@"
